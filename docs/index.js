84a10f3dc487a5fc5195ea21f23f6b3f4de98a03
yassine jemni <151565607+YassineJemni@users.noreply.github.com>
2024-11-19 18:43:53 +0100

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -14,7 +14,7 @@ confirmButton.addEventListener('click', () => {
 
   if (enteredPassword === correctPassword) {
     
-    window.location.href = './hejer/icons/icons.html';
+    window.location.href = 'https://yassinejemni.github.io/icons/icons.html';
   } else {
     
     errorMessage.style.display = 'block';
